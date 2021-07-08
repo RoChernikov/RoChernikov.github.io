0ca3324d288fe6244279724fd644df6c27544a60
Ro Chernikov <imrhubris@gmail.com>
2021-07-08 19:53:18 +0300

    Добавил логику и анимацию открытая PopUp

--- mesto-project/js/script.js
+++ mesto-project/js/script.js
@@ -1,14 +1,16 @@
 "use strict";
+
 const popup = document.querySelector('.popup');
-document.querySelector('.page-btn_type_edit').addEventListener('click', popupOpen);
-document.querySelector('.popup__close-btn').addEventListener('click', popupClose);
-function popupOpen() {
+const popupContainer = document.querySelector('.popup__container')
+
+document.querySelector('.page-btn_type_edit').addEventListener('click', popupOpenClose);
+document.querySelector('.popup__close-btn').addEventListener('click', popupOpenClose);
+
+function popupOpenClose() {
   popup.classList.toggle('popup_opened');
+  popupContainer.classList.toggle('popup__container_opened');
 }
-function popupClose() {
-  popup.classList.toggle('popup_opened');
-}
-// const popup = document.querySelector('.popup');
-// popup.classList.toggle('popup_opened');
+
+
 
 
